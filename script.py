22e98220ddd5e55b9c22f036cc904a3931558545
wuku <80927421+wuku0@users.noreply.github.com>
2022-12-24 12:55:22 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,30 +1,47 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+from bs4 import BeautifulSoup
 
-# Set up the web browser and navigate to tempmail.ninja
-driver = webdriver.Firefox()
-driver.get("https://tempmail.ninja/")
-time.sleep(5)
-# Find the element containing the email address and copy it
-email_element = driver.find_element_by_id("emailtemporal")
+# Open a web browser
+driver = webdriver.Firefox()#replace with Chrome alternatively
+
+
+driver.execute_script("window.open('');")
+driver.switch_to.window(driver.window_handles[1])
+driver.get('https://tempmail.ninja')
+
+# Click the element with the ID "botoncopiar" to get the email
+email_element = driver.find_element_by_id('botoncopiar')
+email = email_element.text
 email_element.click()
-email = email_element.get_attribute("value")
-time.sleep(5)
-# Print the email address
-print(email)
 
-# Navigate to the password generator page
-driver.get("https://www.dashlane.com/features/password-generator")
 
-# Find the password element
-password_element = driver.find_element_by_id("generated-password")
+driver.execute_script("window.open('');")
+driver.switch_to.window(driver.window_handles[2])
+driver.get('https://www.dashlane.com/features/password-generator')
 
-# Copy the password
-password = password_element.get_attribute("value")
+# Click the "Copy password" button to get the password
+password_button = driver.find_element_by_css_selector('.button.button--primary')
+password_button.click()
 
-# Print the password
-print(password)
+# Switch back to the first tab and navigate to the Steam sign-up page
+driver.switch_to.window(driver.window_handles[0])
+driver.get('https://store.steampowered.com/join/')
 
-# Close the browser
-driver.close()
+# Fill out the form on the sign-up page with the email and password
+username_field = driver.find_element_by_id('username')
+username_field.send_keys('my_username')
+email_field = driver.find_element_by_id('email')
+email_field.send_keys(email)
+password_field = driver.find_element_by_id('password')
+password_field.send_keys(password)
+
+# Submit the form
+submit_button = driver.find_element_by_css_selector('button[type="submit"]')
+submit_button.click()
+
+
+print(f'Email: {email}')
+print(f'Password: {password}')
+driver.quit()
+
